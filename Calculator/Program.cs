4837e7e13be468063cafd3becf7e905511187784
Ed Sadler <ed_lorie@comcast.net>
2017-08-08 15:34:02 -0500

    working

--- Calculator/Program.cs
+++ Calculator/Program.cs
@@ -8,5 +8,28 @@ namespace Calculator
         {
             Console.WriteLine("Hello World!");
         }
+
+    }
+    public class Calculator
+    {
+        public int Add(int num1, int num2)
+        {
+            return num1 + num2;
+        }
+
+        public int Subtract(int num3, int num4)
+        {
+            return num3 - num4;
+        }
+
+        public int Multiply(int num5, int num6)
+        {
+            return num5 * num6;
+        }
+
+        public int Divide(int num7, int num8)
+        {
+            return num7 / num8;
+        }
     }
 }
